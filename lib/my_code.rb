587ef533dd0ff9c00458ac74556a77a53ed38a46
Sam Kyprianou <samuelkyprianou@gmail.com>
2019-11-11 16:11:41 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -49,8 +49,8 @@ def reduce_to_total(source_array, starting_point=0)
   counter = 0
   
   while counter < source_array.size do
-  new_array.push(source_array[counter] ** 2)
+  total =
   counter += 1
   end
-  new_array
+  total
 end
