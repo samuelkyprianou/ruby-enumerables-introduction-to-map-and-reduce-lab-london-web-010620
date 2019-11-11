8c95c4d124e6f4ce8329c5a27ec0cab2a22084bd
Sam Kyprianou <samuelkyprianou@gmail.com>
2019-11-11 16:09:09 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -32,3 +32,14 @@ def map_to_double(source_array)
   end
   new_array
 end
+
+def map_to_double(source_array)
+  new_array = []
+  counter = 0
+  
+  while counter < source_array.size do
+  new_array.push(source_array[counter] * 2)
+  counter += 1
+  end
+  new_array
+end
