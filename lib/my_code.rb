7907879433c9a381cb0dff9be0df71ba35b87b87
Sam Kyprianou <samuelkyprianou@gmail.com>
2019-11-11 16:08:06 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,3 +8,15 @@ def map_to_negativize(source_array)
   end
   new_array
 end
+
+
+def map_to_negativize(source_array)
+  new_array = []
+  counter = 0
+  
+  while counter < source_array.size do
+  new_array.push(source_array[counter])
+  counter += 1
+  end
+  new_array
+end
