6046698e2adbd5f9d94e623b27b346471301396b
Sam Kyprianou <samuelkyprianou@gmail.com>
2019-11-11 16:08:55 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -20,3 +20,15 @@ def map_to_no_change(source_array)
   end
   new_array
 end
+
+
+def map_to_no_change(source_array)
+  new_array = []
+  counter = 0
+  
+  while counter < source_array.size do
+  new_array.push(source_array[counter] * 2)
+  counter += 1
+  end
+  new_array
+end
