898fcef1ab40dce969773d11a718c17721bd2466
Sam Kyprianou <samuelkyprianou@gmail.com>
2019-11-11 15:38:56 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -15,5 +15,5 @@ def map_to_square(source_array)
 end
 
 def reduce_to_total(source_array, starting_point)
-  source_array.reduce(starting_point, :+)
+  source_array.reduce(starting_point) { |n| :+)
 end
